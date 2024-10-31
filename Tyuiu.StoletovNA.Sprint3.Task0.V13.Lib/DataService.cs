59b8fb0a62556c6a97f1adae550ff94d13263eff
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-01 01:56:58 +0500

    Add project files.

---
+++ Tyuiu.StoletovNA.Sprint3.Task0.V13.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint3;
+
+namespace Tyuiu.StoletovNA.Sprint3.Task0.V13.Lib
+{
+    public class DataService : ISprint3Task0V13 {
+        public double GetSumSeries(double value, int startValue, int stopValue)
+        {
+            double sumSeries = 0;
+            
+            for (int i = startValue; i <= stopValue; i++)
+            {
+                sumSeries += (Math.Pow(value, i) + 0.5) * Math.Cos(i);
+            }
+            return Math.Round(sumSeries, 3);
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint3.Task0.V13/Program.cs
@@ -0,0 +1,50 @@
+﻿using Tyuiu.StoletovNA.Sprint3.Task0.V13.Lib;
+namespace Tyuiu.StoletovNA.Sprint3.Task0.V13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла for                                                      *");
+            Console.WriteLine("* Задание #0                                                              *");
+            Console.WriteLine("* Вариант #13                                                             *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу используя цикл for, которая вычисляет сумму ряда по фо*");
+            Console.WriteLine("*ормуле.                                                                  *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            double value = 0.5;
+            int startValue = 1;
+            int stopValue = 20;
+
+            Console.WriteLine("Переменная x = " + value);
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = ds.GetSumSeries(value, startValue, stopValue);
+            Console.WriteLine("Сумма ряда = " + result);
+            Console.ReadKey();
+        }
+    }
+}
