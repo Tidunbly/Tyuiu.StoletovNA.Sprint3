402fb0a0cbe608dd9c5fd48666545570b54bcc27
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-01 21:38:27 +0500

    Task 3

---
+++ Tyuiu.StoletovNA.Sprint3.Task3.V6.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.StoletovNA.Sprint3.Task3.V6.Lib;
+namespace Tyuiu.StoletovNA.Sprint3.Task3.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "12able 1o s4gh";
+            char item = 't';
+
+            string wait = "ttable to stgh";
+            string res = ds.ReplaceNumOnChar(value, item);
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint3.Task3.V6/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.StoletovNA.Sprint3.Task3.V6.Lib;
+namespace Tyuiu.StoletovNA.Sprint3.Task3.V6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла foreach                                                  *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #6                                                            *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Используя цикл foreach заменить цифры на букву t в строке: 12able 1o s4gh*");
+            Console.WriteLine("*h                                                                        *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            string value = "12able 1o s4gh";
+            char item = 't';
+
+            Console.WriteLine("Исходная строка = " + value);
+            Console.WriteLine("Символ для замены = " +  item);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = "Количество символов = " + ds.ReplaceNumOnChar(value, item);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
