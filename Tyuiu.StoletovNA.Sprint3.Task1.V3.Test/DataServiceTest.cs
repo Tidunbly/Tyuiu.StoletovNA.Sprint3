ac04759c6b4c62228fd27c7ea1e19adf37207b9a
Никита Столетов <Tyuiu.StoletovNA@outlook.com>
2024-11-01 21:02:28 +0500

    Task1

---
+++ Tyuiu.StoletovNA.Sprint3.Task1.V3.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.StoletovNA.Sprint3.Task1.V3.Lib;
+namespace Tyuiu.StoletovNA.Sprint3.Task1.V3.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 1;
+            int stopValue = 10;
+
+            var res = ds.GetMultiplySeries(startValue, stopValue);
+            Assert.AreEqual(24589.408, res);
+        }
+    }
+}
---
+++ Tyuiu.StoletovNA.Sprint3.Task1.V3/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.StoletovNA.Sprint3.Task1.V3.Lib;
+namespace Tyuiu.StoletovNA.Sprint3.Task1.V3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Спринт #3 | Выполнил: Столетов Никита Андреевич | ИИПБ-24-2";
+            //Длинна строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #3                                                               *");
+            Console.WriteLine("* Оператор цикла while                                                    *");
+            Console.WriteLine("* Задание #1                                                              *");
+            Console.WriteLine("* Вариант #3                                                            *");
+            Console.WriteLine("* Выполнил: Столетов Никита Андреевич | ИИПБ-24-2                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *"); 
+            
+            Console.WriteLine("*Написать программу используя цикл while, которая вычисляет произведение р*");
+            Console.WriteLine("*ряда по формуле                                                          *");
+
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+
+            int startValue = 1;
+            int stopValue = 10;
+
+            Console.WriteLine("Старт шага = " + startValue);
+            Console.WriteLine("Конец шага = " + stopValue);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService ds = new DataService();
+
+            var result = "Произведение ряда = " + ds.GetMultiplySeries(startValue, stopValue);
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
